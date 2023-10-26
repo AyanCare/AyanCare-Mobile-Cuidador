129fa6fcb8f9bd60c7f67a965a5a6a6c4b7c8f16
MillenaFerreira <88831304+MillenaFerreira@users.noreply.github.com>
2023-10-25 23:37:34 -0300

    adicionado o componente DefaultButton

--- app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/components/DefaultButton.kt
+++ app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/components/DefaultButton.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.senai.sp.jandira.ayancare_frontmobile.R
+import br.senai.sp.jandira.ayancare_frontmobile_cuidador.R
 
 @Composable
 
