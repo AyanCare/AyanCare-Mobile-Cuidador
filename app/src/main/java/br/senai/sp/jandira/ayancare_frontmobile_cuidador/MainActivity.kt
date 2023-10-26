ca428dbbd55625429f220c52ccf4945bd19e1bd5
MillenaFerreira <88831304+MillenaFerreira@users.noreply.github.com>
2023-10-25 22:55:04 -0300

    tela colocando os components

--- app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import br.senai.sp.jandira.ayancare_frontmobile_cuidador.ui.theme.AyanCareCuidadorTheme
 
