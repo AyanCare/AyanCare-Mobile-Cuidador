867f7daa3f20f20c5714c82013ed4a24b4d4ca12
GustavoSenai10 <GustavoTenorio010@gmail.com>
2023-11-30 03:19:50 -0300

    Tela de pacientes: TextFild de busca

---
+++ app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/screens/pacientes/components/ResearchField.kt
@@ -0,0 +1,171 @@
+package br.senai.sp.jandira.ayancare_frontmobile.screens.AddRemedy.components
+
+import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.ayancare_frontmobile.retrofit.conectar.ConectarResponse
+import br.senai.sp.jandira.ayancare_frontmobile.retrofit.conectar.service.Conectar
+import br.senai.sp.jandira.ayancare_frontmobile_cuidador.retrofit.RetrofitFactory
+import br.senai.sp.jandira.ayancare_frontmobile_cuidador.screens.Storage
+import br.senai.sp.jandira.ayancare_frontmobile_cuidador.screens.pacientes.components.CardPaciente
+import br.senai.sp.jandira.ayancare_frontmobile_cuidador.sqlite.repository.CuidadorRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+@Composable
+fun ResearchField(
+    localStorage: Storage,
+) {
+    val context = LocalContext.current
+    var isDialogVisibleConect by remember { mutableStateOf(false) }
+    var pacientes by remember { mutableStateOf("") }
+    
+    
+    
+    
+    var listPacientes by remember {
+        mutableStateOf(
+            listOf(
+                Conectar(
+                    0,
+                    "",
+                    0,
+                    "",
+                    "",
+                    0,
+                    ""
+                )
+            )
+        )
+    }
+
+    val array = CuidadorRepository(context = LocalContext.current).findUsers()
+    val paciente = array[0]
+    val id = paciente.id.toLong()
+
+    //Cria uma chamada para o endpoint
+    var call = RetrofitFactory.getConectar().getConectar(id.toInt())
+
+    call.enqueue(object : Callback<ConectarResponse> {
+        override fun onResponse(
+            call: Call<ConectarResponse>,
+            response: Response<ConectarResponse>
+        ) {
+            Log.i("teste de conexao", "onResponse: ${response.body()}")
+            if (response.body()!!.status == 404) {
+                Log.e("TAG", "a resposta está nula")
+                listPacientes = emptyList()
+            } else {
+                listPacientes = response.body()!!.conexao
+            }
+        }
+        override fun onFailure(call: Call<ConectarResponse>, t: Throwable) {
+            Log.i("ds3t", "onFailure: ${t.message}")
+        }
+
+    })
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+                .border(
+                    width = 1.8.dp,
+                    color = Color(167, 165, 164),
+                    shape = RoundedCornerShape(5.dp)
+                ),
+            value = pacientes,
+            onValueChange = { pacientes = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(onClick = { /* Handle icon click if needed */ }) {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        imageVector = Icons.Rounded.Search,
+                        contentDescription = "search",
+                        tint = Color.Black
+                    )
+                }
+            }
+        )
+        
+        Spacer(modifier = Modifier.height(15.dp))
+
+        // Exibir o card do paciente correspondente ao nome digitado
+        val filteredList = listPacientes.filter {
+            it.paciente.lowercase().contains(pacientes.lowercase())
+        }
+
+        if (pacientes.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(15.dp))
+            if (filteredList.isEmpty()) {
+                Text("Paciente não encontrado...")
+            } else {
+                filteredList.firstOrNull()?.let { paciente ->
+                    CardPaciente(
+                        onUnlinkClick = { /*TODO*/ },
+                        onProfileClick = { /*TODO*/ },
+                        nome = paciente.paciente,
+                        id = paciente.id_paciente,
+                        foto = paciente.foto_paciente
+                    )
+                }
+            }
+        } else {
+            LazyColumn() {
+                items(listPacientes) { paciente ->
+                    CardPaciente(
+                        onUnlinkClick = {
+                            isDialogVisibleConect = true
+                        },
+                        onProfileClick = {
+                            localStorage.salvarValor(context, paciente.id_paciente.toString(), "id_paciente_conexao")
+                            //navController.navigate("profile_caregiver_screen")
+                        },
+                        nome = paciente.paciente,
+                        id = paciente.id_paciente,
+                        foto = paciente.foto_paciente,
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+            }
+
+        }
+    }
+}
--- app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/screens/pacientes/screen/PacientesScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile_cuidador/screens/pacientes/screen/PacientesScreen.kt
@@ -4,18 +4,14 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,12 +27,10 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.senai.sp.jandira.ayancare_frontmobile.retrofit.conectar.ConectarResponse
 import br.senai.sp.jandira.ayancare_frontmobile.retrofit.conectar.service.Conectar
-import br.senai.sp.jandira.ayancare_frontmobile.retrofit.patient.PacienteResponse
-import br.senai.sp.jandira.ayancare_frontmobile.retrofit.patient.service.Paciente
+import br.senai.sp.jandira.ayancare_frontmobile.screens.AddRemedy.components.ResearchField
 import br.senai.sp.jandira.ayancare_frontmobile_cuidador.retrofit.RetrofitFactory
 import br.senai.sp.jandira.ayancare_frontmobile_cuidador.screens.Storage
 import br.senai.sp.jandira.ayancare_frontmobile_cuidador.screens.pacientes.components.CardPaciente
-import br.senai.sp.jandira.ayancare_frontmobile_cuidador.screens.settings.screen.contasVinculadas.components.CardLinkedAccounts
 import br.senai.sp.jandira.ayancare_frontmobile_cuidador.sqlite.repository.CuidadorRepository
 import retrofit2.Call
 import retrofit2.Callback
@@ -48,58 +42,10 @@ fun PacienteScreen(
     localStorage: Storage
 ) {
 
-    var isDialogVisibleConect by remember { mutableStateOf(false) }
-    val context = LocalContext.current
-
-    val array = CuidadorRepository(context = context).findUsers()
-
-    val cuidador = array[0]
-    var id = cuidador.id.toLong()
-
-    var listPacientes by remember {
-        mutableStateOf(
-            listOf(
-                Conectar(
-                    0,
-                    "",
-                    0,
-                    "",
-                    "",
-                    0,
-                    ""
-                )
-            )
-        )
-    }
-
-
-    //Cria uma chamada para o endpoint
-    var call = RetrofitFactory.getConectar().getConectar(id.toInt())
-
-    call.enqueue(object : Callback<ConectarResponse> {
-        override fun onResponse(
-            call: Call<ConectarResponse>,
-            response: Response<ConectarResponse>
-        ) {
-            Log.i("teste de conexao", "onResponse: ${response.body()}")
-            if (response.body()!!.status == 404) {
-                Log.e("TAG", "a resposta está nula")
-                listPacientes = emptyList()
-            } else {
-                listPacientes = response.body()!!.conexao
-            }
-        }
-        override fun onFailure(call: Call<ConectarResponse>, t: Throwable) {
-            Log.i("ds3t", "onFailure: ${t.message}")
-        }
-
-    })
-
     Surface(
         color = Color(248, 240, 236)
     ) {
         Column(
-            //verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(top = 40.dp, start = 15.dp, end = 15.dp)
@@ -108,45 +54,11 @@ fun PacienteScreen(
             Text(
                 text = "Meus Pacientes",
                 fontSize = 36.sp,
-                //fontFamily = FontFamily(Font(R.font.poppins)),
                 fontWeight = FontWeight(600),
                 color = Color(0xFF35225F)
             )
-            Spacer(modifier = Modifier.height(25.dp))
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = ""
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 15.dp, end = 15.dp)
-            )
-            Spacer(modifier = Modifier.height(25.dp))
-            LazyColumn(){
-                items(listPacientes){
-                        CardPaciente(
-                            onUnlinkClick = {
-                                isDialogVisibleConect = true
-
-                            },
-                            onProfileClick = {
-                                localStorage.salvarValor(context, it.id_paciente.toString(), "id_paciente_conexao")
-                                navController.navigate("profile_caregiver_screen")
-
-                            },
-                            nome = it.paciente,
-                            id = it.id_paciente,
-                            foto = it.foto_paciente,
-                        )
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-            }
-
+            Spacer(modifier = Modifier.height(15.dp))
+                ResearchField(localStorage = localStorage)
         }
     }
 }
